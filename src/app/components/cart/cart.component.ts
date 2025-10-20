6688fc9b9fe0facdbb2d4b5d875285178a83158b
Mo'men Yasser <momenyaser1120@gmail.com>
2025-10-21 00:21:22 +0300

    Cart Problem Fixed

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -5,6 +5,7 @@ import { CurrencyPipe } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 import { RouterLink } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-cart',
@@ -17,13 +18,14 @@ export class CartComponent implements OnInit, OnDestroy {
   // From Cart Services
   // private readonly _CartService = inject(CartService);
   // private readonly _ToastrService = inject(ToastrService);
-  // make constructor 
+  private readonly spinner = inject(NgxSpinnerService);
+  // make constructor
   constructor(private readonly _CartService: CartService, private readonly _ToastrService: ToastrService){
 
     }
-  
+
   // property to hold the data
-  
+
   cartDetails: Icart = {data:{products:[]},numOfCartItems:0,totalCartPrice:0,status:true,cartId:''} as unknown as Icart;
   // cartDetails: Icart | null = null;
   // Remember to make Sweet Alert
@@ -40,21 +42,23 @@ export class CartComponent implements OnInit, OnDestroy {
 
   private loadCartData(): void {
     this.isLoading = true;
+    this.spinner.show();
     this.CartServiceUnsubscribe = this._CartService.getProductCart().subscribe({
       next: (res) => {
-        console.log(res.data);
+        // console.log(res.data);
         this.cartDetails = res;
         this.isLoading = false;
+        this.spinner.hide();
       },
       error: (err) => {
         this._ToastrService.error('Failed to load cart data', 'Error');
         this.isLoading = false;
-        console.error('Cart loading error:', err);
+        this.spinner.hide();
+        // console.error('Cart loading error:', err);
       },
     });
   }
 
-  // Get out From the Cart
   ngOnDestroy(): void {
     this.CartServiceUnsubscribe?.unsubscribe();
   }
@@ -64,7 +68,7 @@ export class CartComponent implements OnInit, OnDestroy {
     this.isLoading = true;
     this._CartService.removeSpecificCartItem(id).subscribe({
       next: (res) => {
-        console.log(res);
+        // console.log(res);
         this.cartDetails = res;
         this._ToastrService.success("Removed Successfully", "MarketPulse")
         this._CartService.cartCount.set(res.numOfCartItems);
@@ -73,7 +77,7 @@ export class CartComponent implements OnInit, OnDestroy {
       error: (err) => {
         this._ToastrService.error('Failed to remove item', 'Error');
         this.isLoading = false;
-        console.error('Remove item error:', err);
+        // console.error('Remove item error:', err);
       },
     });
   }
@@ -95,7 +99,7 @@ export class CartComponent implements OnInit, OnDestroy {
       error: (err) => {
         this._ToastrService.error('Failed to update cart', 'Error');
         this.isLoading = false;
-        console.error('Update cart error:', err);
+        // console.error('Update cart error:', err);
       },
     });
   }
@@ -105,13 +109,13 @@ export class CartComponent implements OnInit, OnDestroy {
     this.isLoading = true;
     this._CartService.clearUserCart().subscribe({
       next: (res) => {
-        console.log(res);
+        // console.log(res);
         if (res.message == 'success') {
           //fix it
           this.cartDetails = {data:{products:[]},numOfCartItems:0,totalCartPrice:0} as unknown as Icart;
           this._ToastrService.success("Cart Cleared Successfully", "MarketPulse")
-          console.log("done");
-          console.log( this.cartDetails);
+          // console.log("done");
+          // console.log( this.cartDetails);
           this._CartService.cartCount.set(0);
         }
         this.isLoading = false;
@@ -119,7 +123,7 @@ export class CartComponent implements OnInit, OnDestroy {
       error: (err) => {
         this._ToastrService.error('Failed to clear cart', 'Error');
         this.isLoading = false;
-        console.error('Clear cart error:', err);
+        // console.error('Clear cart error:', err);
       },
     });
   }
